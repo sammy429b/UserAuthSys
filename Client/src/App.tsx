cb91dfc0ef1dd4238812f26f1f7a1847d20e2a7c
sammy429b <samarthnbahirgonde49@gmail.com>
2024-04-28 03:18:34 +0530

    reset  password  page design created

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,6 +2,7 @@ import Register from "./pages/Register"
 import Login from "./pages/Login"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import ForgotPassword from "./pages/ForgotPassword"
+import ChangePassword from "./pages/ChangePassword"
 function App() {
   return (
    <>
@@ -9,6 +10,7 @@ function App() {
      <Routes>
       <Route path="/" element={<Login/>}></Route>
       <Route path="/register" element={<Register/>}></Route>
+      <Route path="/password/change" element={<ChangePassword/>}></Route>
       <Route path="/password/reset" element={<ForgotPassword/>}></Route>
      </Routes>
      </BrowserRouter>
--- Client/src/pages/ChangePassword.tsx
+++ Client/src/pages/ChangePassword.tsx
@@ -2,13 +2,17 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-const ForgotPassword = () => {
+const ChangePassword = () => {
   return (
     <>
          <div className="w-full h-screen flex justify-center items-center">
                 <div className="w-1/3 border-2 px-6 py-12 rounded-md shadow-md">
-                    <h1 className="mb-4 text-center text-2xl">Reset Password</h1>
+                    <h1 className="mb-4 text-center text-2xl">Change Password</h1>
                     <form className=" flex flex-col gap-y-4" action="">
+                        <div className="grid w-full max-w-xl items-center gap-1.5">
+                            <Label htmlFor="password">Old Password</Label>
+                            <Input type="password" id="password" placeholder="********" />
+                        </div>
                         <div className="grid w-full max-w-xl items-center gap-1.5">
                             <Label htmlFor="password">Password</Label>
                             <Input type="password" id="password" placeholder="********" />
@@ -17,7 +21,7 @@ const ForgotPassword = () => {
                             <Label htmlFor="password">Re-enter Password</Label>
                             <Input type="password" id="password" placeholder="********" />
                         </div>
-                        <Button className="mt-2">Change Password</Button>
+                        <Button className="mt-2">Reset</Button>
                     </form>
                 </div>
             </div>
@@ -25,4 +29,4 @@ const ForgotPassword = () => {
   )
 }
 
-export default ForgotPassword
+export default ChangePassword
