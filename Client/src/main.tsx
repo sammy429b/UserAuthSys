3c68ba67931bc74f9d7efb9b6564c2fb02cef5a7
sammy429b <samarthnbahirgonde49@gmail.com>
2024-04-28 02:33:34 +0530

    shadcdn installed and register page design created

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from './components/ui/theme-provider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
+  </>,
 )
