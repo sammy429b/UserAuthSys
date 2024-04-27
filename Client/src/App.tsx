0f0fecdab941f869c616be39bd4970acf249dce1
sammy429b <samarthnbahirgonde49@gmail.com>
2024-04-28 03:12:16 +0530

    change password  page design created

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,18 @@
 import Register from "./pages/Register"
-
+import Login from "./pages/Login"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import ForgotPassword from "./pages/ForgotPassword"
 function App() {
   return (
-    <div className="font-bold">
-      <Register/>
-    </div>
+   <>
+     <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Login/>}></Route>
+      <Route path="/register" element={<Register/>}></Route>
+      <Route path="/password/reset" element={<ForgotPassword/>}></Route>
+     </Routes>
+     </BrowserRouter>
+   </>
   )
 }
 
--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,13 +1,18 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+export function TypographyMuted() {
+    return (
+      <p className="text-sm text-muted-foreground">Enter your email address.</p>
+    )
+  }
 
 const Login = () => {
     return (
         <>
             <div className="w-full h-screen flex justify-center items-center">
                 <div className="w-1/3 border-2 px-6 py-12 rounded-md shadow-md">
-                    <h1 className="mb-4 text-center text-2xl">Create New Account</h1>
+                    <h1 className="mb-4 text-center text-2xl font-semibold">Login</h1>
                     <form className=" flex flex-col gap-y-4" action="">
                         <div className="grid w-full max-w-xl items-center gap-1.5">
                             <Label htmlFor="email">Email</Label>
@@ -18,6 +23,7 @@ const Login = () => {
                             <Input type="password" id="password" placeholder="********" />
                         </div>
                         <Button className="mt-2">Login</Button>
+                        <p className="text-sm text-center hover:underline underline-offset-4 duration-200 transition-all">create new account</p>
                     </form>
                 </div>
             </div>
