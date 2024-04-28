5a3f5b41820f067687d2153ae93d13df99711f58
sammy429b <samarthnbahirgonde49@gmail.com>
2024-04-28 14:17:11 +0530

    register form handled using hook form

--- Client/src/pages/Register.tsx
+++ Client/src/pages/Register.tsx
@@ -1,29 +1,43 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import {useForm} from 'react-hook-form'
 
+interface userInputType {
+    username : string,
+    email:string,
+    password:string,
+    repassword: string
+}
+console.log("render of register")
 const Register = () => {
+    const {register, handleSubmit} = useForm<userInputType>();
+
+    const handleLogin = (values: userInputType) =>{
+        console.log("submitted")
+        console.table(values)
+    }
     return (
         <>
             <div className="w-full h-screen flex justify-center items-center">
                 <div className="w-10/12 md:w-1/3 border-2 px-6 py-12 rounded-md shadow-md">
                     <h1 className="mb-4 text-center text-2xl">Create New Account</h1>
-                    <form className=" flex flex-col gap-y-4" action="">
+                    <form className="form flex flex-col gap-y-4" onSubmit={handleSubmit(handleLogin)}>
                         <div className="grid w-full max-w-xl items-center gap-1.5">
                             <Label htmlFor="username">Username</Label>
-                            <Input type="text" id="username" placeholder="abc" />
+                            <Input {...register("username")} type="text" id="username" placeholder="abc" />
                         </div>
                         <div className="grid w-full max-w-xl items-center gap-1.5">
                             <Label htmlFor="email">Email</Label>
-                            <Input type="email" id="email" placeholder="xyz@gmail.com" />
+                            <Input {...register("email")} type="email" id="email" placeholder="xyz@gmail.com" />
                         </div>
                         <div className="grid w-full max-w-xl items-center gap-1.5">
                             <Label htmlFor="password">Password</Label>
-                            <Input type="password" id="password" placeholder="********" />
+                            <Input {...register("password")} type="password" id="password" placeholder="********" />
                         </div>
                         <div className="grid w-full max-w-xl items-center gap-1.5">
-                            <Label htmlFor="password">Re-enter Password</Label>
-                            <Input type="password" id="password" placeholder="********" />
+                            <Label htmlFor="repassword">Re-enter Password</Label>
+                            <Input {...register("repassword")} type="password" id="repassword" placeholder="********" />
                         </div>
                         <Button className="mt-2">Register</Button>
                     </form>
