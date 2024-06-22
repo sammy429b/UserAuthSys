926c6cc028898fa3428cdc62e2ee400c1fba6145
sammy429b <samarthnbahirgonde49@gmail.com>
2024-06-22 16:25:14 +0530

    port and host added for redis client

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -8,12 +8,12 @@ import cookieParser from 'cookie-parser';
 const app : Express = express();
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.SERVER_PORT;
 
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5174',
+    origin: 'http://localhost:5173',
     credentials: true
 }));
 app.use("/",authRoute)
