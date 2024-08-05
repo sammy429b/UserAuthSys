8ae85083be20ed527f53feb2ff6a265d92613922
sammy429b <samarthnbahirgonde49@gmail.com>
2024-08-05 18:49:36 +0530

    http proxy added for avoiding cors error

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,33 +1,44 @@
-import express, {Express, Request,  Response} from 'express'
-import dotenv from 'dotenv'
-import DBconnection from './utils/DBconnect';
-import changePasswordRoute from './routes/changePassword.route'
-import forgotPasswordRoute from './routes/forgotPassword.route'
-import authRoute from './routes/auth.route'
-import cors from 'cors'
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
-const app : Express = express();
+import DBconnection from './utils/DBconnect';
+import changePasswordRoute from './routes/changePassword.route';
+import forgotPasswordRoute from './routes/forgotPassword.route';
+import authRoute from './routes/auth.route';
+import apiProxy from './utils/ProxyServer';
+
+// Load environment variables
 dotenv.config();
 
-const port = process.env.SERVER_PORT;
+const app: Express = express();
+const port = process.env.SERVER_PORT || 5000;
 
+// Middleware'
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(cors({
     origin: 'http://localhost:8080',
     credentials: true
 }));
-app.use("/",authRoute)
-app.use("/",changePasswordRoute)
-app.use("/",forgotPasswordRoute)
 
+// Routes
+app.use("/auth", authRoute);
+app.use("/password", changePasswordRoute);
+app.use("/password", forgotPasswordRoute);
 
-app.get("/", (req: Request, res:Response)=>{
-    res.send("TypeScript wiht express");
-})
+// Proxy
+app.use('/',apiProxy);
 
-app.listen(port || 5000, ()=>{
-    console.log(`http://localhost:${port}`);
-    DBconnection();
-})
+// Base route
+app.get("/", (req: Request, res: Response) => {
+    res.send("TypeScript with express");
+    res.json({ message: "TypeScript with express" });
+});
+
+// Start the server after DB connection
+app.listen(port, async () => {
+    console.log(`Server is running on http://localhost:${port}`);
+    await DBconnection();
+});
